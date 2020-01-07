32f8c95a91b8cb83494545413f030e1bf0014f53
Shawnfeng92 <Shawnfeng92@gmail.com>
2020-01-06 19:45:55 -0600

    Finish MAD optimization with position limiation

--- Optimization/LinearProgramming/MAD.r
+++ Optimization/LinearProgramming/MAD.r
@@ -17,54 +17,109 @@ library(Rglpk)
 returns <- raw.returns[, 1:31]
 
 # Create a min MAD optimization with simple leverage and box constraints ----
-mMad <- list()
-mMad$risk <- "MAD"
+mMAD <- list()
+mMAD$risk <- "MAD"
 
 # Summary parameter
-mMad$scenarios <- nrow(returns)
-mMad$tickers <- ncol(returns)
-mMad$mu <- colMeans(returns)
+mMAD$scenarios <- nrow(returns)
+mMAD$tickers <- ncol(returns)
+mMAD$mu <- colMeans(returns)
 
 # Constraints
-mMad$leverage <- 1.5
-mMad$uL <- 1       # Upper limitation for box constrain
-mMad$lL <- - 1      # Lower limitation
+mMAD$leverage <- 1.5
+mMAD$uL <- 1       # Upper limitation for box constrain
+mMAD$lL <- - 1      # Lower limitation
 
 # Objective function, weight + absolute distance
-mMad$obj <- c(rep(0, mMad$tickers), 
-               rep(1, mMad$scenarios))
+mMAD$obj <- c(rep(0, mMAD$tickers), 
+               rep(1, mMAD$scenarios))
 
 # Constraint Matrix
-mMad$cM <- rbind(
-  c(rep(1, mMad$tickers), rep(0, mMad$scenarios)),
+mMAD$cM <- rbind(
+  c(rep(1, mMAD$tickers), rep(0, mMAD$scenarios)),
   cbind(
     matrix(
-      returns - matrix(rep(mMad$mu, mMad$scenarios), mMad$scenarios), mMad$scenarios
-      ), diag(1, mMad$scenarios)
+      returns - matrix(rep(mMAD$mu, mMAD$scenarios), mMAD$scenarios), mMAD$scenarios
+      ), diag(1, mMAD$scenarios)
   ),
   cbind(
     matrix(
-      - returns + matrix(rep(mMad$mu, mMad$scenarios), mMad$scenarios), mMad$scenarios
-      ), diag(1, mMad$scenarios)
+      - returns + matrix(rep(mMAD$mu, mMAD$scenarios), mMAD$scenarios), mMAD$scenarios
+      ), diag(1, mMAD$scenarios)
   )
 )
 
-
 # Optimization result
-mMad$time <- system.time(
-  mMad$result <- Rglpk_solve_LP(
-    obj = mMad$obj, mat = mMad$cM,
-    dir = c("==", rep(">=", 2 * mMad$scenarios)),
-    bounds = list(lower = list(ind = 1:mMad$tickers,
-                               val = rep(mMad$lL, mMad$tickers)),
-                  upper = list(ind = 1:mMad$tickers,
-                               val = rep(mMad$uL, mMad$tickers))),
-    rhs = c(mMad$leverage, rep(0, 2 * mMad$scenarios)),
+mMAD$time <- system.time(
+  mMAD$result <- Rglpk_solve_LP(
+    obj = mMAD$obj, mat = mMAD$cM,
+    dir = c("==", rep(">=", 2 * mMAD$scenarios)),
+    bounds = list(lower = list(ind = 1:mMAD$tickers,
+                               val = rep(mMAD$lL, mMAD$tickers)),
+                  upper = list(ind = 1:mMAD$tickers,
+                               val = rep(mMAD$uL, mMAD$tickers))),
+    rhs = c(mMAD$leverage, rep(0, 2 * mMAD$scenarios)),
     max = FALSE
   ))
 
 # Outputs
-mMad$weight <- mMad$result$solution[1:mMad$tickers]
-mMad$MAD <- mMad$result$optimum
-names(mMad$weight) <- colnames(returns)
+mMAD$weight <- mMAD$result$solution[1:mMAD$tickers]
+mMAD$MAD <- mMAD$result$optimum
+names(mMAD$weight) <- colnames(returns)
+
+# Create a min MAD optimization with position constraints ----
+mMAD.p <- mMAD
+
+# Add position limitation
+mMAD.p$pL <- 15 
+
+# Objective function, weight + absolute distance + position index
+mMAD.p$obj <- c(rep(0, mMAD.p$tickers), 
+              rep(1, mMAD.p$scenarios), 
+              rep(0, mMAD.p$tickers))
+
+# Constraint Matrix
+mMAD.p$cM <- rbind(
+  c(rep(1, mMAD.p$tickers), rep(0, mMAD.p$scenarios), rep(0, mMAD.p$tickers)),
+  c(rep(0, mMAD.p$tickers), rep(0, mMAD.p$scenarios), rep(1, mMAD.p$tickers)),
+  cbind(
+    matrix(
+      returns - matrix(rep(mMAD.p$mu, mMAD.p$scenarios), mMAD.p$scenarios), mMAD.p$scenarios
+    ), diag(1, mMAD.p$scenarios),
+    matrix(0, mMAD.p$scenarios, mMAD.p$tickers)
+  ),
+  cbind(
+    matrix(
+      - returns + matrix(rep(mMAD.p$mu, mMAD.p$scenarios), mMAD.p$scenarios), mMAD.p$scenarios
+    ), diag(1, mMAD.p$scenarios),
+    matrix(0, mMAD.p$scenarios, mMAD.p$tickers)
+  ),
+  cbind(
+    diag(1, mMAD.p$tickers), matrix(0, mMAD.p$tickers, mMAD.p$scenarios), 
+    diag(-mMAD.p$lL, mMAD.p$tickers)
+  ),
+  cbind(
+    diag(-1, mMAD.p$tickers), matrix(0, mMAD.p$tickers, mMAD.p$scenarios), 
+    diag(mMAD.p$uL, mMAD.p$tickers)
+  )
+)
+
+# Optimization result
+mMAD.p$time <- system.time(
+  mMAD.p$result <- Rglpk_solve_LP(
+    obj = mMAD.p$obj, mat = mMAD.p$cM,
+    dir = c("==", "<=", rep(">=", 2 * mMAD.p$scenarios + 2 * mMAD.p$tickers)),
+    bounds = list(lower = list(ind = 1:mMAD.p$tickers,
+                               val = rep(mMAD.p$lL, mMAD.p$tickers)),
+                  upper = list(ind = 1:mMAD.p$tickers,
+                               val = rep(mMAD.p$uL, mMAD.p$tickers))),
+    rhs = c(mMAD.p$leverage, mMAD.p$pL, rep(0, 2 * mMAD.p$scenarios), rep(-0.005, 2*mMAD.p$tickers)),
+    types = c(rep("C", mMAD.p$tickers + mMAD.p$scenarios), rep("B", mMAD.p$tickers)),
+    max = FALSE
+  ))
+
+# Outputs
+mMAD.p$weight <- mMAD.p$result$solution[1:mMAD.p$tickers]
+mMAD.p$MAD <- mMAD.p$result$optimum
+names(mMAD.p$weight) <- colnames(returns)
 
